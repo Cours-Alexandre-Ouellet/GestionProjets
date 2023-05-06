410f17e1c4cbf56640a85a126794c8ebca29c243
Alexandre Ouellet <36107760+thecakeisreal@users.noreply.github.com>
2023-05-06 12:04:49 -0400

    Creation de l'interface de création de projet

--- src/main/kotlin/com/humaininc/gestionprojets/ControleurCreerProjet.kt
+++ src/main/kotlin/com/humaininc/gestionprojets/ControleurCreerProjet.kt
@@ -1,10 +1,27 @@
 package com.humaininc.gestionprojets
 
+import javafx.fxml.FXML
+
 /**
+ * Contrôleur responsable de la logique de création du projet. Lit les données de l'interface creer_projet
+ * et lance les opérations sur la BD.
  *
+ * Le créateur du projet est le compte connecté.
  *
+ * @param utilisateurConnecte utilisateur qui est actuellement connecte au système.
  * @author Alexandre
  * @since 06/05/2023
  */
-class ControleurCreerProjet {
+class ControleurCreerProjet : ControleurAbstrait() {
+
+   
+
+    @FXML private fun creerProjet() {
+
+    }
+
+    @FXML private fun annuler() {
+        // TODO: revenir à l'interface précédente
+    }
+
 }
